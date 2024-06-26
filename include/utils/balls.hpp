ee8329dde0bbd05ee0f9e3968d849ea314f77571
geostartico <giovanni.artico@gmail.com>
2024-06-26 16:47:51 +0200

    utils

---
+++ include/utils/balls.hpp
@@ -0,0 +1,14 @@
+#ifndef BALLS
+#define BALLS
+#include <opencv2/core.hpp>
+enum BallType{
+    CUE = 1,
+    EIGHT = 2,
+    SOLID = 3,
+    STRIPED = 4
+};
+struct Ball{
+    cv::Rect2d bbox;
+    BallType type; 
+};
+#endif
