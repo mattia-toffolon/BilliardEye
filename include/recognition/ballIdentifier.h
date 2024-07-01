b1f8a338bb95fdb4a0e7a5bd87a1a56ac39d35b9
jolitti <marcodeltrionfo@gmail.com>
2024-07-01 17:06:03 +0200

    completed ball identification (missing 8 ball)

--- include/recognition/ballIdentifier.h
+++ include/recognition/ballIdentifier.h
@@ -10,24 +10,28 @@
 // The purpose of this library is to identify the type of a ball that
 // has been located in a window of an image
 
+// THRESHOLDS FOR BALL FULLNESS
+const float cueballThreshold = 0.3;
+const float stripedThreshold = 0.06;
+
 // Possibly include table color among arguments?
 
 /**
  * @brief Get an estimated BallType from a ball inside an image
  * 
- * @param image Image that contains the ball
- * @param window Precise location of ball
+ * @param ballCrop cropped image containing only the ball
  * @return BallType 
  */
-BallType getBallType(cv::Mat image, cv::Rect2d window);
+BallType getBallType(cv::Mat ballCrop);
 
 /**
- * @brief Estimated percentage of ball which is colored
+ * @brief Estimated percentage of ball in view which is white
  * 
  * @param image Cropped grayscale portion of image containing only the ball
+ * @param thresh Percentile threshold
  * @return float 
  */
-float ballFullness(cv::Mat ballCrop);
+float ballWhiteness(cv::Mat ballCrop, float thresh=0.85);
 
 /**
  * @brief Perform k-means clustering and return associated labels
@@ -56,5 +60,14 @@ std::vector<float> clusterPercentage(cv::Mat img, int k);
  * @return cv::Mat labeled image
  */
 cv::Mat drawClusters(cv::Mat img, int k);
+
+/**
+ * @brief Equalize only part of image corresponding to mask
+ * 
+ * @param img grayscale image to stretch partial contrast of 
+ * @param mask mask to restrict stretching - white pixels only (ball ellipse if empty)
+ * @return cv::Mat partially stretched image
+ */
+cv::Mat equalizedMasked(cv::Mat img, cv::InputArray mask=cv::noArray());
 
 #endif /* BALLTYPE_H */
--- src/recognition/ballIdentifier.cpp
+++ src/recognition/ballIdentifier.cpp
@@ -43,50 +43,39 @@ Mat getBallEllipse(Mat ballCrop)
     return mask;
 }
 
-BallType getBallType(Mat image, Rect2d window)
+BallType getBallType(Mat ballCrop)
 {
     // MAIN IDEA:
     // - convert relevant section of image to greyscale
     // - set the area outside the circle of the ball to black
+    // - stretch the image brightess (localized only on the ball)
     // - threshold the remaining area to determine how much of the ball is white
     // - return an estimation based on this percentage:
     //   - completely white: cueball
     //   - partially white: striped ball
     //   - full or almost full: solid or 8-ball depending on color
     Mat greyCrop;
-    cvtColor(image(window),greyCrop,COLOR_BGR2GRAY);
+    cvtColor(ballCrop,greyCrop,COLOR_BGR2GRAY);
     //Mat isolatedBall;
     //greyCrop.copyTo(isolatedBall,getBallEllipse(greyCrop));
-    float fullness = ballFullness(greyCrop);
+    float whiteness = ballWhiteness(greyCrop);
 
-    return BallType::CUE;
+    if (whiteness > cueballThreshold)
+        return BallType::CUE;
+    else if (whiteness > stripedThreshold)
+        return BallType::STRIPED;
+    else
+        return BallType::SOLID;
 }
 
-float ballFullness(Mat ballCrop)
+float ballWhiteness(Mat ballCrop,float thresh)
 {
-    //Mat equalized;
-    //equalizeHist(ballCrop,equalized);
-    Mat thresholded;
-    threshold(ballCrop,thresholded,0,255,THRESH_BINARY | THRESH_OTSU);
+    Mat transformed = equalizedMasked(ballCrop,noArray());
+    threshold(transformed,transformed,thresh*255,255,THRESH_BINARY);
 
-    Mat isolatedBall;
-    thresholded.copyTo(isolatedBall,getBallEllipse(ballCrop));
-
-    namedWindow("W");
-    Mat scaled;
-    resize(ballCrop,scaled,Size(),10,10);
-    imshow("W",scaled);
-    waitKey(0);
-    resize(isolatedBall,scaled,Size(),10,10);
-    imshow("W",scaled);
-    waitKey(0);
-
-    // next part should be adapted to the inscribed circle of the area
-    int nonZero = countNonZero(isolatedBall);
-    int area = ballCrop.size[0] * ballCrop.size[1];
-
-    // return share of crop that is not black
-    return (area - nonZero)/area;
+    int whites = countNonZero(transformed);
+    int totalPixels = transformed.rows * transformed.cols;
+    return (float)whites / (float)totalPixels;
 }
 
 // -----------------
@@ -148,4 +137,49 @@ Mat drawClusters(Mat img, int k)
             newImg.at<uchar>(r,c) = (labels[index]/(k-1))*255;
         }
     return newImg;
+}
+
+Mat equalizedMasked(Mat img, InputArray mask)
+{
+    Mat output;
+
+    Mat matMask;
+    if (mask.empty())
+    {
+        matMask = getBallEllipse(img);
+    }
+    else
+        matMask = mask.getMat();
+
+    img.copyTo(output);
+    uchar min = 255;
+    uchar max = 0;
+    for (int r=0; r<img.cols; r++)
+    {
+        for (int c=0; c<img.cols; c++)
+        {
+            if (matMask.at<uchar>(r,c))
+            {
+                min = std::min(min,img.at<uchar>(r,c));
+                max = std::max(max,img.at<uchar>(r,c));
+            }
+        }
+    }
+    //std::cout << (int)min << " " << (int)max << "\n";
+    for (int r=0; r<img.cols; r++)
+    {
+        for (int c=0; c<img.cols; c++)
+        {
+            if (matMask.at<uchar>(r,c))
+            {
+                uchar p = img.at<uchar>(r,c);
+                float pp = (float)(p-min)/(float)(max-min);
+                //std::cout << pp*255 << "\n";
+                output.at<uchar>(r,c) = pp*255;
+            }
+            else
+                output.at<uchar>(r,c) = 0;
+        }
+    }
+    return output;
 }
