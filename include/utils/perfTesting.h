28cd8fd85874fb78592142af56a942a21a5db456
jolitti <marcodeltrionfo@gmail.com>
2024-07-04 13:02:33 +0200

    added IoU

--- include/utils/perfTesting.h
+++ include/utils/perfTesting.h
@@ -1,5 +1,18 @@
 #ifndef PERFTESTING_H
 #define PERFTESTING_H
 
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include "utils/balls.hpp"
+
+/**
+ * @brief Calculates IoU (percentage) of two `Rect`s
+ * 
+ * @param region1 first rectangle to compare
+ * @param region2 second rectangle to compare
+ * @return float IoU
+ */
+float intersectionOverUnion(cv::Rect region1, cv::Rect region2);
 
 #endif /* PERFTESTING_H */
--- src/utils/perfTesting.cpp
+++ src/utils/perfTesting.cpp
@@ -1,2 +1,10 @@
 #include "utils/perfTesting.h"
 
+using namespace cv;
+
+float intersectionOverUnion(Rect region1, Rect region2)
+{
+    float areaInters = (region1 & region2).area();
+    float areaUnion = region1.area() + region2.area() - areaInters;
+    return areaInters / areaUnion;
+}
