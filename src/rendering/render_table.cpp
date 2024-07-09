b81d828cf0d18043ce415a141256482d6b31331b
geostartico <giovanni.artico@gmail.com>
2024-07-09 16:25:33 +0200

    correct render

--- src/rendering/render_table.cpp
+++ src/rendering/render_table.cpp
@@ -1,4 +1,5 @@
 #include "rendering/render_table.hpp"
+#include "opencv2/imgproc.hpp"
 #include "utils/drawBBoxes.hpp"
 #include <algorithm>
 using namespace cv;
@@ -11,16 +12,16 @@ Scalar getColor(BallType b){
         return Scalar(0, 0, 0);
     }
     else if(b == BallType::SOLID){
-        return Scalar(255, 0, 255);
+        return Scalar(255, 0, 0);
     }
     else{
-        return Scalar(255, 255, 0);
+        return Scalar(0, 0, 255);
     }
 }
 
 TableRenderer::TableRenderer(VideoReader v, TrackBalls t, std::vector<Ball> balls, cv::Mat transform, int width, int cols) : tracker(t), vid(v), transform(transform.clone()){
     this->curimg = Mat::zeros(Size(width, cols), CV_8UC3);
-    curimg.setTo(Scalar(125, 125,125));
+    curimg.setTo(Scalar(255, 255,255));
     for(auto r : balls){
         Ball curb{r.bbox, r.type};
         bbs.push_back(curb);
@@ -29,18 +30,21 @@ TableRenderer::TableRenderer(VideoReader v, TrackBalls t, std::vector<Ball> ball
 
 cv::Mat TableRenderer::nextFrame(){
     Mat fram = this->vid.nextFrame();
-    if(fram.rows == 0) return fram;
-    const std::vector<Ball> newballs = tracker.update(fram);
+    if(fram.rows == 0){
+        return fram;
+    }
+    std::vector<int> removed;
+    const std::vector<Ball> newballs = tracker.update(fram, removed);
     std::vector<Rect> bounding;
     for(auto b : newballs){
         bounding.push_back(b.bbox);
     }
     drawBBoxes(fram, bounding);
+    int real = 0;
     for(int i = 0; i < bbs.size(); i++){
-        if(newballs[i].bbox.x < 0){
-            bbs[i].bbox = Rect(-1,-1,0,0);
+        if(std::find(removed.begin(), removed.end(), i) != removed.end()){
             continue;
-        }
+        }    
         Point2f oldcenter = Point2f(bbs[i].bbox.x+bbs[i].bbox.width/2,bbs[i].bbox.y+bbs[i].bbox.height/2);
         Point2f newCenter = Point2f(newballs[i].bbox.x+newballs[i].bbox.width/2,newballs[i].bbox.y+newballs[i].bbox.height/2);
         std::vector<Point2f> old{oldcenter};
@@ -48,7 +52,12 @@ cv::Mat TableRenderer::nextFrame(){
         perspectiveTransform(old, old, transform);
         perspectiveTransform(niu, niu, transform);
         line(curimg, old[0], niu[0], Scalar(0,0,0));
-        bbs[i].bbox = newballs[i].bbox;
+        bbs[i].bbox = newballs[real].bbox;
+        real++;
+    }
+    for(int i = removed.size()-1; i >=0; i--){
+        bbs.erase(bbs.begin()+removed[i],bbs.begin()+removed[i]+1);
+        bbs.erase(bbs.begin()+removed[i],bbs.begin()+removed[i]+1);
     }
     Mat screen = curimg.clone();
     for(Ball b : bbs){
@@ -59,6 +68,7 @@ cv::Mat TableRenderer::nextFrame(){
         std::vector<Point2f> vec{c};
         perspectiveTransform(vec, vec, transform);
         circle(screen, vec[0], 10, getColor(b.type), FILLED);
+        circle(screen, vec[0], 11, Scalar(255,255,255), 2, LINE_AA);
     }
     return screen;
 }
